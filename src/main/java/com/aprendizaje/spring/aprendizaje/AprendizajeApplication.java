b6f12408cb7e70faadbfe1751adff1f2dfebf82e
Alejo <alejito23001@gmail.com>
2022-08-22 14:53:31 -0500

    Init

---
+++ src/main/java/com/aprendizaje/spring/aprendizaje/AprendizajeApplication.java
@@ -0,0 +1,13 @@
+package com.aprendizaje.spring.aprendizaje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendizajeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendizajeApplication.class, args);
+	}
+
+}
